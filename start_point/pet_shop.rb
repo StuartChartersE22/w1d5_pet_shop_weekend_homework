634e7a50252eb785a392fae55d90eea9581f9892
StuartCharters <charters.stuart@gmail.com>
2018-05-25 10:49:51 +0100

    3rd test passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,3 +5,7 @@ end
 def total_cash(pet_shop_details)
   return pet_shop_details[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop_details, amount_to_change_by)
+  pet_shop_details[:admin][:total_cash] += amount_to_change_by
+end
