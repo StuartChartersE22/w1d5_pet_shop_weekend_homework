16439bef1b6b42e690920e9883c1407626af1fc0
StuartCharters <charters.stuart@gmail.com>
2018-05-25 15:24:25 +0100

    labeled each function for which test number

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,27 +1,34 @@
+#1st test
 def pet_shop_name(pet_shop_details)
   return pet_shop_details[:name]
 end
 
+# 2nd test
 def total_cash(pet_shop_details)
   return pet_shop_details[:admin][:total_cash]
 end
 
+#3rd, 4th test
 def add_or_remove_cash(pet_shop_details, amount_to_change_by)
   pet_shop_details[:admin][:total_cash] += amount_to_change_by
 end
 
+#5th test
 def pets_sold(pet_shop_details)
   return pet_shop_details[:admin][:pets_sold]
 end
 
+#6th test
 def increase_pets_sold(pet_shop_details,amount_to_change_by)
   pet_shop_details[:admin][:pets_sold] += amount_to_change_by
 end
 
+#7th test
 def stock_count(pet_shop_details)
   return pet_shop_details[:pets].length()
 end
 
+#8th, 9th test
 def pets_by_breed(pet_shop_details, breed_to_be_found)
   collection_of_pets = []
   for pet in pet_shop_details[:pets]
@@ -32,6 +39,7 @@ def pets_by_breed(pet_shop_details, breed_to_be_found)
   return collection_of_pets
 end
 
+#10th, 11th test
 def find_pet_by_name(pet_shop_details, name_to_be_found)
   for pet in pet_shop_details[:pets]
     if pet[:name] == name_to_be_found
@@ -41,6 +49,7 @@ def find_pet_by_name(pet_shop_details, name_to_be_found)
   return nil
 end
 
+#12th test
 def remove_pet_by_name(pet_shop_details, name_to_remove)
   for pet in pet_shop_details[:pets]
     if pet[:name] == name_to_remove
@@ -49,10 +58,17 @@ def remove_pet_by_name(pet_shop_details, name_to_remove)
   end
 end
 
+#13th test
 def add_pet_to_stock(pet_shop_details, pet_to_add)
   pet_shop_details[:pets].push(pet_to_add)
 end
 
+#14th test
 def customer_cash(customer)
   return customer[:cash]
+end
+
+#15th test
+def remove_customer_cash(customer, amount_to_change_by)
+  customer[:cash] -= amount_to_change_by
 end
