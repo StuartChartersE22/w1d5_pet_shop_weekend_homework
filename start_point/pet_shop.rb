5cf4127d420e9510df9329f8ded3d38b46d934a6
StuartCharters <charters.stuart@gmail.com>
2018-05-25 11:09:24 +0100

    7th test passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -13,3 +13,7 @@ end
 def pets_sold(pet_shop_details)
   return pet_shop_details[:admin][:pets_sold]
 end
+
+def increase_pets_sold(pet_shop_details,amount_to_change_by)
+  pet_shop_details[:admin][:pets_sold] += amount_to_change_by
+end
